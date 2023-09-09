63db98d4f8b5c143fd35374de8c0146c049868ba
Richard Udoh <udohrichard201114@gmail.com>
2023-09-09 17:44:18 +0200

    Category Page Design

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Countdown from 'react-countdown'
 
-const endingDate = new Date('2023-09-03')
+const endingDate = new Date('2023-10-6')
 const CountDown = () => {
   return (
     <Countdown className='font-bold text-5xl text-yellow-300' date ={endingDate}/>
